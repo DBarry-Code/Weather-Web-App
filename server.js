051d0bcc1d764810fff870db64e4018857a4a154
DBarry-Code <dabrowski.barry@gmail.com>
2020-02-11 18:47:50 +0100

    master

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(express.static('public'))
   
   app.post('/weather', (req, res) => {
-   console.log(req.Body)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}`
+    axios({
+      url: url,
+      responseType: 'json'
+    }).then(data => res.json(data.data.currently))
   })
   
   app.listen(3000, () => {
